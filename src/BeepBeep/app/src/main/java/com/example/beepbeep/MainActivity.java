ad7f14104171c2baa745104b5250f488a16bb2bd
Triang1e <xiyuan1@ualberta.ca>
2020-03-04 04:27:33 -0700

    geolocation2
    working on map that get the position of user

--- src/BeepBeep/app/src/main/java/com/example/beepbeep/MainActivity.java
+++ src/BeepBeep/app/src/main/java/com/example/beepbeep/MainActivity.java
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // start main activity
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, MapsActivity.class);
+//        startActivity(intent);
 
     }
 }
--- src/BeepBeep/app/src/main/java/com/example/beepbeep/MapsActivity.java
+++ src/BeepBeep/app/src/main/java/com/example/beepbeep/MapsActivity.java
@@ -1,8 +1,22 @@
 package com.example.beepbeep;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-import android.os.Bundle;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,19 +27,48 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = "MapsActivity";
+    private static final int ERROR = 9901;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Construct a GeoDataClient.
-//        mGeoDataClient = Places.getGeoDataClient(this, null);
-
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        //get current location
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        LocationListener locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                Toast.makeText(
+//                        getBaseContext(),
+//                        "Location changed: Lat: " + location.getLatitude() + " Lng: "
+//                                + location.getLongitude(), Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String s, int i, Bundle bundle) { }
+//
+//            @Override
+//            public void onProviderEnabled(String s) { }
+//
+//            @Override
+//            public void onProviderDisabled(String s) { }
+//        };
+//        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            // Permission is not granted
+//        }
+//        else {
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+//        }
     }
 
 
@@ -47,4 +90,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    public boolean isServicesRight(){
+        Log.d(TAG,"?");
+        int isAvaliable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapsActivity.this);
+
+        if(isAvaliable == ConnectionResult.SUCCESS){
+            //everything is fine
+            Log.d(TAG,"google play services is okay");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(isAvaliable)){
+            //something wrong but it can resolve
+            Log.d(TAG,"an error occur");
+        }
+        else {
+            Toast.makeText(this,"We can't conncet to map",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return false;
+
+    }
+
 }
