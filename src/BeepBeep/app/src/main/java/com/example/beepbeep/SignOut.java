38fa27be542db20a32f34e417b7b5335f3ae7687
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2020-03-05 11:35:31 -0700

    Signout function added and tested

---
+++ src/BeepBeep/app/src/main/java/com/example/beepbeep/SignOut.java
@@ -0,0 +1,32 @@
+package com.example.beepbeep;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.io.File;
+
+class SignOut {
+    static void now(Context c){
+        // delete all shared preferences saved on the machine
+        File sharedPreferenceFile = new File(c.getFilesDir().getParentFile().getAbsolutePath() + File.separator + "shared_prefs");
+        File[] listFiles = sharedPreferenceFile.listFiles();
+        if(listFiles != null){
+            for (File file : listFiles) {
+                boolean a = file.delete();
+            }
+        }else{
+            Log.d("Acount", "fuck");
+        }
+
+        // relaunch app
+        Intent mStartActivity = new Intent(c, MainActivity.class);
+        int mPendingIntentId = 123456;
+        PendingIntent mPendingIntent = PendingIntent.getActivity(c, mPendingIntentId,    mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager mgr = (AlarmManager)c.getSystemService(Context.ALARM_SERVICE);
+        mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+        System.exit(0);
+    }
+}
