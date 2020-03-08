e7f1b5e96f36a4b00f488ec30f138e8d1c638375
Lospub <zhjyy002@gmail.com>
2020-03-08 17:22:09 -0600

    connect firebase

--- src/BeepBeep/app/src/main/java/com/example/beepbeep/RiderMapActivity.java
+++ src/BeepBeep/app/src/main/java/com/example/beepbeep/RiderMapActivity.java
@@ -1,4 +1,21 @@
 package com.example.beepbeep;
 
-public class RiderMapActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RiderMapActivity extends AppCompatActivity {
+    final String TAG = "Account";
+    FirebaseDatabase database;
+    DatabaseReference ref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_main);
+        ref = FirebaseDatabase.getInstance().getReference().child(TAG);
+
+    }
 }
